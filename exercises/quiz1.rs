b5b8158e316e7878b5dfed492d50231778efac7d
Alexander Jaworowski <alexander@jaworowski.se>
2023-02-06 13:58:53 +0000

    Finished quiz 1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,11 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(n:i32) -> i32 {
+    let p = if n > 40 {1} else {2};
+    n*p
+}
 
 // Don't modify this function!
 #[test]
